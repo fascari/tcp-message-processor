1f4d5690c5fa5dc782cbda6d0bbe1ca21f1dfe6f
felipe.ascari <felipe.ascari@gmail.com>
2026-01-20 17:55:16 -0300

    refactor: simplify rate limit integration test

--- client/test/integration/ratelimit_test.go
+++ client/test/integration/ratelimit_test.go
@@ -7,65 +7,50 @@ import (
 	"testing"
 	"time"
 
-	"tcp-message-processor-client/internal/transport"
+	"tcp-message-processor-client/testsuite"
 	"tcp-message-processor/common/pkg/closer"
 	"tcp-message-processor/common/pkg/hash"
-	"tcp-message-processor/common/pkg/integration"
 	"tcp-message-processor/common/pkg/tcp"
 	apperrors "tcp-message-processor/pkg/errors"
 
-	"github.com/stretchr/testify/require"
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/suite"
 )
 
 type RateLimitSuite struct {
-	suite.Suite
-	integrationSuite *integration.Suite
+	testsuite.Integration
 }
 
 func TestRateLimitSuite(t *testing.T) {
 	suite.Run(t, new(RateLimitSuite))
 }
 
-func (s *RateLimitSuite) SetupSuite() {
-	s.integrationSuite = integration.NewSuite()
-	err := s.integrationSuite.Setup()
-	require.NoError(s.T(), err, "failed to setup integration suite")
-}
-
-func (s *RateLimitSuite) TearDownSuite() {
-	if s.integrationSuite != nil {
-		s.integrationSuite.Teardown()
-	}
-}
-
 func (s *RateLimitSuite) TestRateLimiting() {
-	conn, err := transport.Connect(s.integrationSuite.ServerHost, s.integrationSuite.ServerPort)
+	netConn, err := s.Connect()
 	s.Require().NoError(err)
-	defer closer.Close(conn, "failed to close connection")
+	defer closer.Close(netConn, "failed to close connection")
 
-	username := fmt.Sprintf("ratelimit-test-%d", time.Now().UnixNano())
+	conn := tcp.NewConn(netConn)
+	s.authenticate(conn, fmt.Sprintf("ratelimit-test-%d", time.Now().UnixNano()))
 
-	s.authenticate(conn, username)
 	jobID, serverNonce := s.waitForJob(conn)
-
 	accepted, rejected := s.attemptSubmissions(conn, jobID, serverNonce)
 
-	s.T().Logf("Results: %d accepted, %d rejected", accepted, rejected)
+	s.T().Logf("Rate limit test: %d accepted, %d rejected", accepted, rejected)
 	s.Greater(rejected, 0)
 	s.LessOrEqual(accepted, 2)
 }
 
-func (s *RateLimitSuite) authenticate(conn transport.Connection, username string) {
-	authMsg := tcp.AuthorizeParams{Username: username}.ToMessage(1)
-	s.Require().NoError(conn.Write(authMsg))
+func (s *RateLimitSuite) authenticate(conn *tcp.Conn, username string) {
+	msg := tcp.AuthorizeParams{Username: username}.ToMessage(1)
+	s.Require().NoError(conn.Write(&msg))
 
 	resp, err := conn.Read()
 	s.Require().NoError(err)
 	s.Require().Empty(resp.Error)
 }
 
-func (s *RateLimitSuite) waitForJob(conn transport.Connection) (int64, string) {
+func (s *RateLimitSuite) waitForJob(conn *tcp.Conn) (int64, string) {
 	msg, err := conn.Read()
 	s.Require().NoError(err)
 	s.Require().True(msg.IsJob())
@@ -76,7 +61,7 @@ func (s *RateLimitSuite) waitForJob(conn transport.Connection) (int64, string) {
 	return jobID, serverNonce
 }
 
-func (s *RateLimitSuite) readResponse(conn transport.Connection) tcp.Message {
+func (s *RateLimitSuite) readResponse(conn *tcp.Conn) tcp.Message {
 	for {
 		resp, err := conn.Read()
 		s.Require().NoError(err)
@@ -89,33 +74,24 @@ func (s *RateLimitSuite) readResponse(conn transport.Connection) tcp.Message {
 	}
 }
 
-func (s *RateLimitSuite) attemptSubmissions(conn transport.Connection, jobID int64, serverNonce string) (int, int) {
-	accepted := 0
-	rejected := 0
-
-	s.T().Log("Attempting 5 rapid submissions")
+func (s *RateLimitSuite) attemptSubmissions(conn *tcp.Conn, jobID int64, serverNonce string) (int, int) {
+	accepted, rejected := 0, 0
 
 	for i := 0; i < 5; i++ {
-		errMsg := s.submitOnce(conn, jobID, serverNonce, i)
-		if errMsg == "" {
+		if s.submitOnce(conn, jobID, serverNonce, i) {
 			accepted++
-			s.T().Logf("  Attempt %d: ACCEPTED", i+1)
-			continue
-		}
-
-		rejected++
-		s.T().Logf("  Attempt %d: REJECTED - %s", i+1, errMsg)
-		s.Equal(apperrors.ErrSubmissionFrequent.Error(), errMsg)
-
-		if i < 4 {
-			time.Sleep(200 * time.Millisecond)
+		} else {
+			rejected++
+			if i < 4 {
+				time.Sleep(200 * time.Millisecond)
+			}
 		}
 	}
 
 	return accepted, rejected
 }
 
-func (s *RateLimitSuite) submitOnce(conn transport.Connection, jobID int64, serverNonce string, attempt int) string {
+func (s *RateLimitSuite) submitOnce(conn *tcp.Conn, jobID int64, serverNonce string, attempt int) bool {
 	nonce := fmt.Sprintf("nonce-%d-%d", time.Now().UnixNano(), attempt)
 	result := hash.SHA256(serverNonce + nonce)
 
@@ -125,8 +101,12 @@ func (s *RateLimitSuite) submitOnce(conn transport.Connection, jobID int64, serv
 		Result:      result,
 	}.ToMessage(int64(attempt + 2))
 
-	s.Require().NoError(conn.Write(submitMsg))
+	s.Require().NoError(conn.Write(&submitMsg))
 
 	resp := s.readResponse(conn)
-	return resp.Error
+	if resp.Error != "" {
+		s.Equal(apperrors.ErrSubmissionFrequent.Error(), resp.Error)
+		return false
+	}
+	return true
 }
